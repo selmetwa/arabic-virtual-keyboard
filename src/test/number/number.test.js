279742bc0b380253c933e76cafb1e16e520e7ccb
Sherif Elmetwally <sge5@njit.edu>
2023-11-05 16:26:30 -0500

    update number tests

--- src/test/number/number.test.js
+++ src/test/number/number.test.js
@@ -1,5 +1,6 @@
 import { expect } from '@esm-bundle/chai';
-import { isNumber } from '../../helpers/number.js';
+import { numbers } from '../../constants/data.js';
+import { isNumber, numberFactory } from '../../helpers/number.js';
 
 describe('isNumber', () => {
   it('should return true if the value is a number', () => {
@@ -16,5 +17,47 @@ describe('isNumber', () => {
 
   it('should return false if the value is null', () => {
     expect(isNumber(null)).to.be.false;
+  });
+});
+
+describe('numberFactory function', () => {
+  it('should return the corresponding Arabic number based on the English number input', () => {
+    const englishNumber = '1';
+    const expectedArabicNumber = '\u0661';
+
+    const arabicNumber = numberFactory(englishNumber);
+
+    expect(arabicNumber).to.equal(expectedArabicNumber);
+  });
+
+  it('should handle a non-existing English number', () => {
+    const englishNumber = '10';
+
+    const arabicNumber = numberFactory(englishNumber);
+
+    expect(arabicNumber).to.be.undefined;
+  });
+
+  it('should handle empty input', () => {
+    const englishNumber = '';
+
+    const arabicNumber = numberFactory(englishNumber);
+
+    expect(arabicNumber).to.be.undefined;
+  });
+
+  it('should return undefined for undefined input', () => {
+    const englishNumber = undefined;
+
+    const arabicNumber = numberFactory(englishNumber);
+
+    expect(arabicNumber).to.be.undefined;
+  });
+
+  it('should return the corresponding Arabic number for each English number', () => {
+    numbers.forEach(number => {
+      const arabicNumber = numberFactory(number.en);
+      expect(arabicNumber).to.equal(number.ar);
+    });
   });
 });
