e03bae72b73aaaf8085188e7528e9e93828dca40
Sherif Elmetwally <sge5@njit.edu>
2023-11-09 21:51:47 -0500

    handle insertion

--- src/helpers/number.js
+++ src/helpers/number.js
@@ -3,19 +3,24 @@ import { numbers } from "../constants/data.js";
 /**
  * Function to return the corresponding arabic number based on the english number input.
  * @param {string} englishNumber - The english number to be converted to arabic.
+ * @param {Element} text - The text element to be updated, we need the cursor position and current text
  * @returns {string} arabicNumber - The arabic number.
  */
-export const numberFactory = (englishNumber) => {
+export const numberFactory = (englishNumber, originalText, cursorPosition) => {
+  const originalArray = originalText.split('');
   const numberObject = numbers.find((obj) => obj.en === englishNumber);
   const arabicNumber = numberObject && numberObject.ar;
-  return arabicNumber;
+
+  originalArray.splice(cursorPosition - 1, 0, arabicNumber);
+  const newText = originalArray.join('');
+  return { newText, newCursorPosition: cursorPosition - 1 };
 };
 
 /** 
  * Function to check if the value is a number.
  * @param {string} value - The value to be checked.
  * @returns {boolean} - True if the value is a number, false otherwise.
-*/
+ */
 export const isNumber = (value) => {
   return [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].includes(parseInt(value));
 };
