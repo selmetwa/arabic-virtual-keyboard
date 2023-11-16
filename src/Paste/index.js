f6785251c1dc40c5ecb9d966c19d5276e20e4bdc
Sherif Elmetwally <sge5@njit.edu>
2023-11-15 22:14:43 -0500

    add jsdoc comments and accomodate pasting from keyboard

--- src/Paste/index.js
+++ src/Paste/index.js
@@ -1,11 +1,27 @@
-import { deleteSelectedText } from "../Backspace/index.js";
-import { isNumber } from '../utils.js'
+import * as Types from '../constants/types.js'
+import { isNumber, deleteSelectedText, isInputArabic } from '../utils.js'
 import { numberFactory } from '../Numbers/index.js'
 
-export const PasteFactory = (event, state) => {
-  const pastedText = (event.clipboardData).getData("text");
+/**
+ * 
+ * @param {Event} event - paste event
+ * @param {string=} pastedTextFromClipboard - pasted text from clipboard
+ * @param {Types.state} state - current state of the keyboard
+ */
+export const PasteFactory = (event, pastedTextFromClipboard, state) => {
+  // console.log('PasteFactory', event, pastedTextFromClipboard, state)
+  const pastedText = event && event.clipboardData && event.clipboardData.getData("text") || pastedTextFromClipboard;
+  const isArabic = isInputArabic(pastedText)
+
+  if (isArabic) {
+    return {
+      textValue: pastedText,
+    }
+  }
+
+  console.log({ isArabic })
+  alert(pastedText)
   const _selectedText = state.selectedText;
-  console.log('pastedText', pastedText)
   let _cursorPosition = state.cursorPosition;
   let _textValue = state.textValue;
 
@@ -17,6 +33,7 @@ export const PasteFactory = (event, state) => {
 
   for (let i = 0; i < pastedText.length; i++) {
     const char = pastedText[i];
+
     if (isNumber(char)) {
       const { newText } = numberFactory(char, _textValue, _cursorPosition);
       _cursorPosition = _cursorPosition + 1;
