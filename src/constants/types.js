ce9c5d75b36d42b96f7d14553d29d61baa2fcb61
Sherif Elmetwally <sge5@njit.edu>
2023-11-15 22:14:00 -0500

    create types file

---
+++ src/constants/types.js
@@ -0,0 +1,9 @@
+/**
+ * @typedef {Object} State - Current state of the keyboard
+ * @property {number} cursorPosition - cursor position
+ * @property {string} textValue - text value
+ * @property {string} previousKey - previous key pressed
+ * @property {string} selectedText - selected text
+ */
+
+export const Types = {}
